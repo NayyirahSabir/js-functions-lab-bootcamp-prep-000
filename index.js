75a4f107bb89ec4b6f65aced66293a2dcaf8f902
nsabir <awesomeartist15@gmail.com>
2019-06-17 22:15:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,19 +7,14 @@ function happyHolidaysTo(name){
   console.log("Happy holidays, ${name}!")()
 }
 
-happyHolidaysTo("you")
-
 function happyHolidayTo(holiday, name){
   console.log("Happy ${holiday}, ${name}!")
 }
-
-happyHolidayTo("Independence Day", "you")
 
 function holidayCountdown(days, holiday){
   console.log("It's ${days} days until ${holiday}!")
 }
 
-holidayCountdown("20", "Mother's Day")
 
 
 
